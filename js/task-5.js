63e5e2c2f23dabe9633c0579b95bed028ac4eba8
Roman <postromanben@gmail.com>
2025-09-22 00:02:33 +0300

    add task-5

--- js/task-5.js
+++ js/task-5.js
@@ -1,3 +1,15 @@
+const bodyEl = document.querySelector("body");
+const spanEl = document.querySelector(".color");
+const btnEl = document.querySelector(".change-color");
+
+btnEl.addEventListener("click", changeBackgraund);
+
+function changeBackgraund() {
+  const color = getRandomHexColor();
+  bodyEl.style.backgroundColor = color;
+  spanEl.textContent = color;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
